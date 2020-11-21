90b2e26041dca07b607803db3f3080b9051fa075
opeolluwa <adefemih214@yahoo.com>
2020-11-21 18:36:04 +0100

    updated

---
+++ quot0.10.js
@@ -0,0 +1,36 @@
+const _$QUOT = {
+    init: function(nodeId) {
+   
+     const $quot = () => document.getElementById(nodeId);
+     //get started
+     let data = fetch('/data.json')
+      //parse
+      .then(data => data.json())
+      //define and refernce variables
+      .then((data) => {
+       //define index holder
+       let index = Math.round(data.length * Math.random());
+   
+       //a temporary holder
+       fetchedData = {
+        quoteContent: [data[index][0], index],
+        quoterContent: [data[index][1], index]
+       };
+       //afterwards accept object
+      }).then(() => {
+       //create nodes
+       let quote = document.createElement('div');
+       let quoter = document.createElement("small");
+       //afterwards add content to nodes
+       quote.appendChild(document.createTextNode(fetchedData.quoteContent));
+       quoter.appendChild(document.createTextNode(fetchedData.quoterContent));
+       //add stylesheet 
+       quote.classList.add('quot');
+       quoter.classList.add('quotr');
+       //then add to holder
+       console.log($quot(nodeId))
+       $quot(nodeId).appendChild(quote)
+       $quot(nodeId).appendChild(quoter)
+      })
+    }
+   }
