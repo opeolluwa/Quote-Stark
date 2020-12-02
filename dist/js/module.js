3e3e0759ca08a53e0ee02b32bcdc1733db5e2635
opeolluwa <adefemih214@yahoo.com>
2020-12-02 14:07:37 +0100

    update

--- dist/js/module.js
+++ dist/js/module.js
@@ -1,24 +1,32 @@
+//import quotes
 import data from './data.js';
+//refrence the root node 
 let node = document.querySelector('[data-quot="node"]');
+//define children nodrs
 let quote = document.createElement('div');
 let quoter = document.createElement("small");
+//get quote randomly #index
+ let index = [Math.round(data.length * Math.random())];
+ //to start with : append quote and quoter on respective nodes 
+ quote.innerHTML = data[index][0];
+ quoter.innerHTML = data[index][1];
+ //add stylesheet
 node.appendChild(quote);
 node.appendChild(quoter);
-
-function drx() {
-
-
- let index = [Math.round(data.length * Math.random())];
-
- console.log(index)
-
- quote.innerHTML = data[index][0];
-
- quoter.innerHTML = data[index][1];
-
- //add stylesheet 
+//append children nodes to root node
  quote.classList.add('quot');
  quoter.classList.add('quotr');
+/*define a function that repeat the process
+=> get index
+=> get quote at index 
+=> append quote and quoter to nodes
+=> append quote and quoter nodes to root node
+*/
+function $quot() {
+ let index = [Math.round(data.length * Math.random())];
+ console.log(index)
+ quote.innerHTML = data[index][0];
+ quoter.innerHTML = data[index][1];
 }
 
-window.setInterval(drx, 1000);
+window.setInterval($quot, 10000);
