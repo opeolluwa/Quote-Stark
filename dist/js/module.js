7dc99018b56e835da0ace2fde1682bcb06a843dd
opeolluwa <adefemih214@yahoo.com>
2020-12-25 19:25:24 +0100

    minoe update and bug fixes

--- dist/js/module.js
+++ dist/js/module.js
@@ -6,7 +6,7 @@ let node = document.querySelector('[data-quot="node"]');
 let quote = document.createElement('div');
 let quoter = document.createElement("small");
 //get quote randomly #index
- let index = [Math.round(data.length * Math.random())];
+ let index = [Math.round((data.length -1)* Math.random())];
  //to start with : append quote and quoter on respective nodes 
  quote.innerHTML = data[index][0];
  quoter.innerHTML = data[index][1];
@@ -15,6 +15,7 @@ node.appendChild(quote);
 node.appendChild(quoter);
 //append children nodes to root node
  quote.classList.add('quot');
+ //quote.classList.add('atz');
  quoter.classList.add('quotr');
 /*define a function that repeat the process
 => get index
@@ -27,6 +28,21 @@ function $quot() {
  console.log(index)
  quote.innerHTML = data[index][0];
  quoter.innerHTML = data[index][1];
+ //animate node
+ node.animate([
+  //keyframes
+   { filter: 'blur(5px)' },
+   { opacity: 0 },
+   { filter: 'blur(0)' },
+   { opacity: 1 }],
+  {
+   // timing options
+   duration: 1000,
+   delay:100,
+   easing:'cubic-bezier(.55, .085, .68, .53)',
+   fill:'both'
+  });
 }
 
-window.setInterval($quot, 10000);
+window.setInterval($quot, 15000);
+//window.setInterval(quote.classList.add('atz'), 10000)
